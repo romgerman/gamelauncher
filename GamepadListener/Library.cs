d10cd33f75a45b9fea990dcfe7be984e2cb80fa0
unpause <roidl.sebastian@gmail.com>
2017-08-23 13:11:16 +0200

    Started working on the library database stuff

--- GamepadListener/Library.cs
+++ GamepadListener/Library.cs
@@ -6,6 +6,30 @@ using System.Threading.Tasks;
 
 namespace GamepadListener
 {
+    class LibraryItemApplication : LibraryItem
+    {
+        string PlayCount { get; set; }
+        string LastPlayed { get; set; }
+    }
+
+    class LibraryItemFolder : LibraryItem
+    {
+
+    }
+
+    class LibraryItem
+    {
+        string Path { get; set; }
+        string Name { get; set; }
+        string Desc { get; set; }
+        string Thumbnail { get; set; }
+    }
+
+    class LibraryData
+    {
+
+    }
+
     class Library
     {
     }
