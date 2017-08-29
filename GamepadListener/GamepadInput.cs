04fa1098a706004b10e37380b889406a09e33bac
romgerman <romgerman@users.noreply.github.com>
2017-08-29 17:45:42 +0300

    Nothing

---
+++ GamepadListener/GamepadInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamepadListener
+{
+    class GamepadInput
+    {
+        // TODO: here we somehow should organize UI and input
+    }
+}
